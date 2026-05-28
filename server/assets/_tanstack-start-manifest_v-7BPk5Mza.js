78fcfddd77ede17e1c01aa8effedf54883afc4d9
hyun <rlaus233@gmail.com>
2026-05-29 07:08:25 +0900

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-7BPk5Mza.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "C:/Users/a/Desktop/rgeo-builds-zero-main/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-DPC8SLKO.js"] }, "/": { filePath: "C:/Users/a/Desktop/rgeo-builds-zero-main/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-CISDbzJS.js"] } }, clientEntry: "/assets/index-DPC8SLKO.js" });
+export {
+  tsrStartManifest
+};
---
+++ server/index.js
@@ -0,0 +1,5 @@
+import { w } from "./assets/worker-entry-Br3GJl6R.js";
+import "node:events";
+export {
+  w as default
+};
